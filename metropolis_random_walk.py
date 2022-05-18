169811a20a8dd23ca0ba312f21be07af648e6de3
Axel Nilsson <Karlaxel.n@outlook.com>
2022-05-18 21:45:42 +0200

    added simulator.

--- metropolis_random_walk.py
+++ metropolis_random_walk.py
@@ -1,12 +1,55 @@
 import numpy as np
 import pandas as pd
 import statsmodels.api as sm
+import logging
 
+from scipy.stats import multivariate_normal, bernoulli
 
 bid_data = np.loadtxt('data/eBayNumberOfBidderData.dat', unpack=True)
 columns = ['nBids', 'Const', 'PowerSeller', 'VerifyID',  'Sealed',  'Minblem',
            'MajBlem', 'LargNeg', 'LogBook', 'MinBidShare']
 df = pd.DataFrame(bid_data.T, columns=columns)
+
+
+def my_posterior(params: np.array) -> float:
+    return multivariate_normal.pdf(params, poisson_regression.hessian(params))
+
+
+class MetropolisRandomWalk:
+    def __init__(self, posterior_function, init_sample: np.array, step_lenght: float, covariance_matrix: np.array):
+        self.posterior_function = posterior_function
+        self.init_sample = init_sample
+        self.step_lenght = step_lenght
+        self.covariance_matrix = step_lenght * covariance_matrix
+
+    def simulate_params(self, size: int = 100) -> list:
+        samples = []
+        prev_sample = self.init_sample
+
+        for _ in range(size):
+            current_sample = multivariate_normal.rvs(prev_sample,
+                                                     self.covariance_matrix,
+                                                     1
+                                                     )
+            try:
+                beta = self.posterior_function(
+                    current_sample)/self.posterior_function(prev_sample)
+            except TypeError as e:
+                logging.critical(e, exc_info=True)
+            except ValueError as e:
+                logging.critical(e, exc_info=True)
+            finally:
+                print('H')
+
+            alpha = np.min(1, beta)
+            decision = bernoulli.rvs(alpha, size=1)
+            if decision:
+                current_sample = prev_sample
+
+            samples.append(current_sample)
+
+        return samples
+
 
 if __name__ == '__main__':
     poisson_regression = sm.GLM(df['nBids'],
@@ -21,7 +64,7 @@ if __name__ == '__main__':
     appr_posterior_cov_matrix = np.linalg.inv(mode_hessian)
     print(appr_posterior_cov_matrix)
 
-
-class MetropolisRandomWalk:
-    def __init__(self, posterior_function: function, init_sample: np.array):
-        pass
+    init_sample = np.ones(9)
+    metropolis_sampler = MetropolisRandomWalk(
+        my_posterior, init_sample=init_sample, step_lenght=1, covariance_matrix=mode_hessian)
+    samples = metropolis_sampler.simulate_params()
